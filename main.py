1992a5b80d5dc9014a90f5d3459fdd77021706b0
wklein1 <Witali@>
2022-10-29 00:44:50 +0200

    #4 refactor get currences endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
+from models import currency_models, error_models
 import deta
 
 PROJECT_KEY = config("PROJECT_KEY")
@@ -26,8 +27,12 @@ app.add_middleware(
 
 @app.get(
     "/currencies",
+    response_model=list[currency_models.CurrencyModel],
     response_description="Returns list of available currencies",
     description="Get all available currencies.",    
 )
 async def get_currencies():
-    return currenciesDB.fetch().items
+    try:
+        return currenciesDB.fetch().items
+    except:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Error while connecting to database")
